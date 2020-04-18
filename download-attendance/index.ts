b809c1bce32e09ed4f72d5f7cf57abac8db094ff
aoisupersix <aoisupersix@gmail.com>
2020-04-18 10:00:40 +0900

    [add] start confirmation of puppeteer

--- download-attendance/index.ts
+++ download-attendance/index.ts
@@ -1,4 +1,5 @@
 import { AzureFunction, Context, HttpRequest } from '@azure/functions'
+import * as puppeteer from 'puppeteer'
 
 const httpTrigger: AzureFunction = async function (
   context: Context,
@@ -6,6 +7,14 @@ const httpTrigger: AzureFunction = async function (
 ): Promise<void> {
   context.log('HTTP trigger function download-attendance processed a request.')
   const name = req.query.name || (req.body && req.body.name)
+  const browser = await puppeteer.launch({
+    args: ['--no-sandbox', '--disable-setuid-sandbox'],
+  })
+  const page = await browser.newPage()
+  await page.goto('https://id.jobcan.jp/users/sign_in')
+  await page.screenshot({ path: 'screenshot.png' })
+
+  await browser.close()
 
   if (name) {
     context.res = {
